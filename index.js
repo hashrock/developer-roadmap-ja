8a8004068efd99ba084533e5971db8f129b02475
hashrock <hashrock@users.noreply.github.com>
2019-01-22 00:34:31 +0900

    fix: improve style

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ Vue.component("box", {
     <a :href="link">
       <text x=150 y=20 dominant-baseline="central" text-anchor="middle">{{label}}</text>
     </a>
-    <rect @click="check" fill="white" stroke="black" x=5 y=5 width=20 height=20>
+    <rect class="box__checkbox" @click="check" fill="white" stroke="black" x=5 y=5 width=20 height=20>
     </rect>
-    <path v-if="checked" fill="none" stroke="blue" stroke-width="3" d="M 7,17 L 10,20 L 20,10"></path>
+    <path class="box__checkmark" v-if="checked" fill="none" stroke="blue" stroke-width="3" d="M 7,17 L 10,20 L 20,10"></path>
   </g>
   `,
   props: {
@@ -50,12 +50,13 @@ Vue.component("box", {
 
 Vue.component("tree", {
   template: `<div>
-  <svg style="margin: 2rem" width="800" :height="height" :viewBox="viewBox">
-    <box :storage="storage" :x="0" :y="0" :label="label" type="parent"></box>
+  <svg style="margin: 2rem" width="680" :height="height" :viewBox="viewBox">
     <g v-for="(item, idx) in children">
+      <path stroke-linecap="round" fill="none" stroke-width=3 stroke="blue" stroke-dasharray="0.1,10" :d="curve(idx)"></path>
       <box :storage="storage" :x="360" :y="idx * 60" :label="item"></box>
-      <line stroke="blue" stroke-dasharray="5,5" x1=300 y1=20 x2=360 :y2="20 + 60 * idx"></line>
+      <line x1=300 y1=20 x2=360 :y2="20 + 60 * idx"></line>
     </g>
+    <box :storage="storage" :x="0" :y="0" :label="label" type="parent"></box>
   </svg>
   </div>`,
   props: {
@@ -63,12 +64,17 @@ Vue.component("tree", {
     children: Array,
     storage: Array
   },
+  methods: {
+    curve(idx) {
+      return `M 300,20 C 330,20 330,${20 + 60 * idx} 360,${20 + 60 * idx}`;
+    }
+  },
   computed: {
     height() {
       return this.children.length * 60;
     },
     viewBox() {
-      return `0 0 800 ${this.height}`;
+      return `0 0 680 ${this.height}`;
     }
   }
 });
@@ -92,40 +98,15 @@ new Vue({
   el: "#app",
   data: {
     storage: [],
-    treedata: {
-      title: "基礎",
-      tree: [
-        {
-          label: "HTML",
-          children: [
-            "HTMLの基礎",
-            "セマンティックなHTMLを書く",
-            "基本的なSEO",
-            "アクセシビリティ"
-          ]
-        },
-        {
-          label: "CSS",
-          children: [
-            "CSSの基礎",
-            "レイアウトを作る",
-            "メディアクエリ",
-            "CSS3を学ぶ"
-          ]
-        },
-        {
-          label: "JavaScriptの基礎",
-          children: [
-            "基礎的な文法",
-            "DOM操作を学ぶ",
-            "Fetch APIを学ぶ",
-            "ES6とJavaScriptの部品化を学ぶ"
-          ]
-        }
-      ]
-    }
+    treedata: {}
   },
   mounted() {
+    fetch("./roadmap-frontend.json")
+      .then(i => i.json())
+      .then(d => {
+        this.treedata = d;
+      });
+
     const local = localStorage.getItem(LOCALSTORAGE_KEY);
     if (local) {
       this.storage = JSON.parse(local);
